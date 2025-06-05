3244244d0d1f4836e12617cc0fa4cf3555c58a3f
joscCE <j.campos@estudiantec.cr>
2025-06-04 23:22:57 -0600

    falta_probar

--- CPU.sv
+++ CPU.sv
@@ -17,12 +17,12 @@ assign WriteData = 32'b0;
 
 
 logic [31:0] PC, next_PC, PC_4, PC_8;
-logic [31:0] RD1;
+logic [31:0] RD1, RD2, WD3;
 logic [31:0] Extd;
 logic [31:0] ALUResult;
 logic ALUZero;
 assign PC_Addres = PC;
-
+assign WriteData = RD2;
 
 Register #(.N(32)) PC_Register(
 	.clk(clk), 
@@ -35,14 +35,14 @@ Register #(.N(32)) PC_Register(
 
 Register_File #(.N(4), .M(32)) Reg_file(
     .A1(Instruction[19:16]),
-    .A2(),
+    .A2(Instruction[15:12]),
     .A3(Instruction[15:12]),
     .clk(clk),
     .WE3(1'b1),
     .R15(PC_8),
     .RD1(RD1),
-    .RD2(), 
-    .WD3(readData)
+    .RD2(RD2), 
+    .WD3(WD3)
 );
 
 
@@ -62,8 +62,6 @@ ALU #(.N(32)) ALU_ins(
 );
 
 
-
-
 Adder #(.N(32)) PCPlus4(
     .A(32'd4),
     .B(PC),
@@ -80,11 +78,18 @@ Adder #(.N(32)) PCPlus8(
 
 Mux #(.N(32)) PCsource(
     .A(readData),
-    .B(PC_4),
-	.S(1'b0),
+    .B(ALUResult),
+	.S(1'b1),
 	.C(next_PC)
 
 );
 
 
+Mux #(.N(32)) MemToReg(
+    .A(readData),
+    .B(PC_4),
+	.S(1'b0),
+	.C(WD3)
+
+);
 endmodule
