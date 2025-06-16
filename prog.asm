5cca01a366a8f1e1e830736472fda3e800624030
JBB092 <jos.barquero@estudiantec.cr>
2025-06-15 19:32:05 -0600

    [UPDATE]Cambio archivos asm

--- prog.asm
+++ prog.asm
@@ -1,5 +1,9 @@
 .global _start
 _start:
-    ADD R3, R1, R2
-    ADD R3, R1, R2 
-    ADD R3, R1, R2  
+
+    LDR R1, [R2, #0]    
+    ADD R3, R1, R1       
+    LDR R4, [R2, #4]     
+    ADD R5, R3, R4       
+    ADD R6, R5, R5       
+    B .                  
