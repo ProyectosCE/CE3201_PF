08632e9299827069ef6e8c88ef4ea3579b761bc0
joscCE <j.campos@estudiantec.cr>
2025-06-16 21:08:48 -0600

    expansion_memoria

--- Data_Memory.sv
+++ Data_Memory.sv
@@ -1,24 +1,36 @@
 module Data_Memory(
+    input  logic        clk,
 
-	input logic clk,we,
-	input logic [31:0] a,wd,
-	
-	output logic [31:0] rd
-	
+    // Acceso principal
+    input  logic        we,
+    input  logic [31:0] a,
+    input  logic [31:0] wd,
+    output logic [31:0] rd,
 
-	);
-	
-	
- logic[31:0]RAM[63:0];
- 
- assign rd =RAM[a[31:2]];
- 
- 
- always_ff@(posedge clk)
- 
- if(we) 
-   RAM[a[31:2]]<=wd;
- 
- 
- endmodule
- 
+    // Acceso desde teclado (escritura)
+    input  logic        we_kb,
+    input  logic [31:0] addr_kb,
+    input  logic [31:0] data_kb,
+
+    // Acceso desde VGA (lectura)
+    input  logic [31:0] addr_vga,
+    output logic [31:0] data_vga
+);
+
+    logic [31:0] RAM [0:63];
+
+    // Escritura principal
+    always_ff @(posedge clk) begin
+        if (we)
+            RAM[a[31:2]] <= wd;
+        else if (we_kb)
+            RAM[addr_kb[31:2]] <= data_kb;
+    end
+
+    // Lectura principal
+    assign rd = RAM[a[31:2]];
+
+    // Lectura VGA
+    assign data_vga = RAM[addr_vga[31:2]];
+
+endmodule
